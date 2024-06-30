6aab6e3435387f41310ea9fcc0174604b7231094
Mudream <mudream4869@gmail.com>
2024-06-30 10:13:19 +0800

    Add doc to echo

--- toolgui/component/tcmisc/echo.go
+++ toolgui/component/tcmisc/echo.go
@@ -60,6 +60,25 @@ func removeIndent(lines []string) []string {
 	return ret
 }
 
+// Echo will execute lambda and show the code in the lambda.
+// To use Echo, we need to store the code in advance (usually by embedded).
+//
+//	//go:embed main.go
+//	var code string
+//	// ...
+//	// ok, echo will execute and show `component.Text(c, "hello echo")`
+//	tcmisc.Echo(c, code, func() {
+//		component.Text(c, "hello echo")
+//	})
+//
+//	// panic, since Echo only parse code line by line
+//	tcmisc.Echo(c, code, func() {component.Text(c, "hello echo")})
+//
+//	// panic, since Echo only parse code that start from caller
+//	myFunc := func() {
+//		component.Text(c, "hello echo")
+//	}
+//	tcmisc.Echo(c, code, myFunc)
 func Echo(c *framework.Container, code string, lambda func()) {
 	_, filename, line, ok := runtime.Caller(1)
 	if !ok {
